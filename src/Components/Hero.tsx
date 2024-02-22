95a9eb0f84ece26c25df77b300f44574de4e1d4c
Pi4a <piralith@outlook.fr>
2024-02-22 10:07:37 +0100

    navbar

---
+++ src/Components/Hero.tsx
@@ -0,0 +1,25 @@
+function Hero() {
+  return (
+    <>
+      <div className="hero">
+        <div className="hero-text">
+          <h4>Plan your trip now</h4>
+          <h1>
+            Save <span className="text-orange-400>">big</span> with our car
+            rental{" "}
+          </h1>
+          <p>
+            Rent the car of your dreams.Unbeatable prices, unlimited miles,
+            flexible pick-up options and much more.
+          </p>
+        </div>
+        <div className="hero-btns">
+          <button className=""></button>
+          <button className=""></button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+import logo from "../assets/logo/logo.0ad964cb93ab30cc809d.png"
+import { RxHamburgerMenu } from "react-icons/rx"
+import { IoCloseOutline } from "react-icons/io5"
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const openMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  return (
+    <nav className="flex justify-between items-center h-28 px-5">
+      <img src={logo} className="w-[9rem]" />
+      <RxHamburgerMenu className="size-8" onClick={openMenu} />
+      <div
+        className={
+          menuOpen
+            ? "w-full h-[100dvh] absolute top-0 left-0 ease-in-out duration-500 z-10 bg-white flex justify-center items-center"
+            : "w-full h-[100dvh] absolute -left-full top-0 ease-in-out duration-500 z-10 bg-white flex justify-center items-center"
+        }
+      >
+        <IoCloseOutline
+          className="absolute right-3 top-8 size-12"
+          onClick={openMenu}
+        />
+        <ul className="text-2xl font-semibold text-center space-y-8">
+          <li>Home</li>
+          <li>About</li>
+          <li>Models</li>
+          <li>Testimonials</li>
+          <li>Our team</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
