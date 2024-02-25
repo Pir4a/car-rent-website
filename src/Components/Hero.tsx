993e493873a964522b256ee80bff1cf344eb198f
Pi4a <piralith@outlook.fr>
2024-02-25 15:37:22 +0100

    navbar and hero desktops

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import { FaCar } from "react-icons/fa6"
 import { MdNavigateNext } from "react-icons/md"
 import { FaCircleArrowUp } from "react-icons/fa6"
+import maincar from "../assets/hero/main-car.png"
+import herobg from "../assets/hero/hero-bg.png"
 
 function Hero() {
   const [goUp, setGoUp] = useState(false)
@@ -27,29 +29,39 @@ function Hero() {
 
   return (
     <>
-      <div className="hero font-rubik bg-gray pt-28 pb-36">
-        <div className="hero-text flex flex-col justify-center gap-4 items-center text-center px-4 py-12">
-          <h4 className="font-bold text-2xl">Plan your trip now</h4>
-          <h1 className="font-bold text-large">
-            Save <span className="text-orange font-normal>">big</span> with our
-            car rental{" "}
-          </h1>
-          <p className="opacity-70 text-lg">
-            Rent the car of your dreams. Unbeatable prices, unlimited miles,
-            flexible pick-up options and much more.
-          </p>
+      <div className="hero font-rubik bg-gray pt-28 pb-36 lg:flex lg:items-center lg:pt-12 lg:min-h-dvh lg:justify-center lg:gap-16">
+        <img
+          src={herobg}
+          className="hidden lg:block absolute right-0 max-w-[50%] min-h-[90dvh]"
+        />
+        <div className="2xl:w-1/4 xl:w-2/4 max-h-[500px] z-20 lg:pl-8 lg:pt-16">
+          <div className="hero-text flex flex-col justify-center gap-4 items-center text-center px-4 py-12 lg:text-left lg:p-0">
+            <h4 className="font-bold text-2xl w-full">Plan your trip now</h4>
+            <h1 className="font-bold text-large">
+              Save <span className="text-orange font-normal>">big</span> with
+              our car rental{" "}
+            </h1>
+            <p className="opacity-70 text-lg">
+              Rent the car of your dreams. Unbeatable prices, unlimited miles,
+              flexible pick-up options and much more.
+            </p>
+          </div>
+          <div className="hero-btns flex flex-col justify-center gap-4 items-center mt-6 lg:flex-row lg:max-w-[400px]">
+            <a
+              href="#carbook"
+              className="flex items-center gap-3 w-2/3 h-16 justify-center rounded bg-orangelight shadow-md shadow-orange text-white font-bold"
+            >
+              Book Ride <FaCar />
+            </a>
+            <a className="flex items-center gap-1 w-2/3 h-16 justify-center bg-black text-white font-bold">
+              Learn More <MdNavigateNext className="size-6" />
+            </a>
+          </div>
         </div>
-        <div className="hero-btns flex flex-col justify-center gap-4 items-center mt-6">
-          <a
-            href="#carbook"
-            className="flex items-center gap-3 w-2/3 h-16 justify-center rounded bg-orangelight shadow-md shadow-orange text-white font-bold"
-          >
-            Book Ride <FaCar />
-          </a>
-          <a className="flex items-center gap-1 w-2/3 h-16 justify-center bg-black text-white font-bold">
-            Learn More <MdNavigateNext className="size-6" />
-          </a>
-        </div>
+        <img
+          src={maincar}
+          className="hidden lg:block lg:max-w-[40%] py-12 z-20"
+        />
       </div>
       <div
         onClick={scrollToTop}
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -12,45 +12,87 @@ function Navbar() {
   }
 
   return (
-    <nav className="bg-transparent absolute top-0 left-0 right-0 flex justify-between items-center h-28 px-5 z-50">
-      <img src={logo} className="w-[9rem]" />
-      <RxHamburgerMenu
-        className={menuOpen ? "size-8 text-orange" : "size-8"}
-        onClick={openMenu}
-      />
-      <div
-        className={
-          menuOpen
-            ? "w-full h-[100dvh] absolute top-0 left-0 ease-in-out duration-500 z-10 bg-white flex justify-center items-center"
-            : "w-full h-[100dvh] absolute -left-full top-0 ease-in-out duration-500 z-10 bg-white flex justify-center items-center"
-        }
-      >
-        <IoCloseOutline
-          className="absolute right-3 top-8 size-12"
+    <>
+      <nav className="bg-transparent absolute top-0 left-0 right-0 flex justify-between items-center h-28 px-5 z-50 lg:hidden">
+        <img src={logo} className="w-[9rem]" />
+        <RxHamburgerMenu
+          className={menuOpen ? "size-8 text-orange" : "size-8"}
           onClick={openMenu}
         />
-        <ul className="text-2xl font-semibold text-center space-y-8">
-          <li onClick={openMenu}>
-            <Link to="/">Home</Link>
-          </li>
-          <li onClick={openMenu}>
-            <Link to="/about">About</Link>
-          </li>
-          <li onClick={openMenu}>
-            <Link to="/models">Models</Link>
-          </li>
-          <li onClick={openMenu}>
-            <Link to="/testimonials">Testimonials</Link>
-          </li>
-          <li onClick={openMenu}>
-            <Link to="/team">Our team</Link>
-          </li>
-          <li onClick={openMenu}>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <div
+          className={
+            menuOpen
+              ? "w-full h-[100dvh] absolute top-0 left-0 ease-in-out duration-500 z-10 bg-white flex justify-center items-center"
+              : "w-full h-[100dvh] absolute -left-full top-0 ease-in-out duration-500 z-10 bg-white flex justify-center items-center"
+          }
+        >
+          <IoCloseOutline
+            className="absolute right-3 top-8 size-12"
+            onClick={openMenu}
+          />
+          <ul className="text-2xl font-semibold text-center space-y-8">
+            <li onClick={openMenu}>
+              <Link to="/">Home</Link>
+            </li>
+            <li onClick={openMenu}>
+              <Link to="/about">About</Link>
+            </li>
+            <li onClick={openMenu}>
+              <Link to="/models">Models</Link>
+            </li>
+            <li onClick={openMenu}>
+              <Link to="/testimonials">Testimonials</Link>
+            </li>
+            <li onClick={openMenu}>
+              <Link to="/team">Our team</Link>
+            </li>
+            <li onClick={openMenu}>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      {/*desktop */}
+
+      <nav className="hidden lg:flex lg:justify-center bg-transparent absolute  top-0 left-0 right-0 min-h-28 px-5 z-50">
+        <div className="flex justify-between lg:gap-9 xl:gap-4 items-center xl:w-3/4">
+          <div>
+            <Link to="/">
+              <img src={logo} className="w-[9rem]" />
+            </Link>
+          </div>
+          <ul className="flex gap-8 lg:text-lg xl:text-xl font-semibold text-center">
+            <li className="hover:text-orange transition-all duration-300 ease-in-out">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="hover:text-orange transition-all duration-300 ease-in-out">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="hover:text-orange transition-all duration-300 ease-in-out">
+              <Link to="/models">Models</Link>
+            </li>
+            <li className="hover:text-orange transition-all duration-300 ease-in-out">
+              <Link to="/testimonials">Testimonials</Link>
+            </li>
+            <li className="hover:text-orange transition-all duration-300 ease-in-out">
+              <Link to="/team">Our team</Link>
+            </li>
+            <li className="hover:text-orange transition-all duration-300 ease-in-out">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <div className="flex gap-4">
+            <button className="font-semibold text-lg hover:text-orange transition-all duration-300 ease-in-out">
+              Sign in
+            </button>
+            <button className="h-12 w-28 rounded bg-orange text-lg text-white font-semibold shadow-md">
+              Register
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
 
